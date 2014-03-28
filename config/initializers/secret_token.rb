0a75d9308cec33d03a2516c2ae38904e27629447
Jackie Herrlin <j.m.herrlin@gmail.com>
2014-03-27 17:22:53 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+OmniauthLab::Application.config.secret_key_base = 'ce7e45ec55ca3eb20c6a6ce955f5f2b4ce1c4ac80d26b7b10089bd0a50794de15550612265c35b96451829b52b9d5bbfcb3d133c12eba74bd5a7b2866a4bcbb7'
